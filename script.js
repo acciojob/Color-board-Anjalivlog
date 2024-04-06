b6a6d2039c1fb801f3fb78a0b4a204f49365a6e7
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-06 14:04:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+
+// Generate 799 more square boxes
+for (let i = 0; i < 799; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+}
