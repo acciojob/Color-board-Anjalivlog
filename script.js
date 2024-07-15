09596769239b3d48f68f2a274b0a4892e64eddad
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-07-15 14:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 //your JS code here. If required.
 const container = document.querySelector('.container');
-
-// Generate 799 more square boxes
-for (let i = 0; i < 799; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+for (let i = 0; i < 800; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  container.appendChild(square);
 }
+container.addEventListener('mouseover', (event) => {
+  if (event.target.classList.contains('square')) {
+    event.target.style.backgroundColor = randomColorFunction();
+    setTimeout(() => {
+      event.target.style.backgroundColor = 'rgb(29, 29, 29)';
+    }, 1000);
+  }
+});
+
+function randomColorFunction() {
+    const r = Math.floor(Math.random() * 256); // Random between 0-255
+    const g = Math.floor(Math.random() * 256); // Random between 0-255
+    const b = Math.floor(Math.random() * 256); // Random between 0-255
+    return 'rgb(' + r + ',' + g + ',' + b + ')'; 
+}   
